be3ae61eb38c2a2b427c98a66e7ed7b73589ef36
Mateo Marthoz Blanco <mateo.marthoz_blanco@kcl.ac.uk>
2024-10-17 22:16:25 +0100

    Implement median calculation

--- median.py
+++ median.py
@@ -1,6 +1,17 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def find_median(numbers):
+    sorted_numbers = sorted(numbers)
+    length = len(sorted_numbers)
+
+    if length % 2 == 1: # Odd number of elements
+        return sorted_numbers[length // 2]
+    else: # Even number of elements
+        middle1 = sorted_numbers[length // 2 - 1]
+        middle2 = sorted_numbers[length // 2]
+        return (middle1 + middle2) / 2
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +20,6 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+median = find_median(numbers)
+print(f"The median is: {median}")
